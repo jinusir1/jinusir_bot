54ddb62f09e23774e4cd245d08ce123b906089fb
aju001 <89148418+aju001@users.noreply.github.com>
2021-09-17 14:06:36 +0530

    Update zara.js

--- zara.js
+++ zara.js
@@ -1,4 +1,4 @@
-/* codded by afnanplk
+/* codded by aju
 */
 
 const { Sequelize } = require('sequelize');
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
- Z_NUMBER: process.env.NUMBER === undefined ? '+994400602992' : process.env.NUMBER,
- Z_OWNER: process.env.OWNER_NAME === undefined ? 'Raashii' : process.env.OWNER_NAME,
+ Z_NUMBER: process.env.NUMBER === undefined ? '+13072881866' : process.env.NUMBER,
+ Z_OWNER: process.env.OWNER_NAME === undefined ? 'Aju ser' : process.env.OWNER_NAME,
 
 };
