7d1105fad9a4f0fe237070fb58d7c82c9e97aea7
aju001 <89148418+aju001@users.noreply.github.com>
2021-09-17 13:51:19 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     
     
    
-  r_text[0] = "https://ibb.co/x3hn5bG";
+  r_text[0] = "https://i.ibb.co/8KWTjMn/20210917-093455.jpg";
     
     
     var i = Math.floor(1*Math.random())
