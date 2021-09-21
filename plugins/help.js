3e4bad8498e8000d5c755c11fddbbd601e1bda7a
Aju <89148418+aju001@users.noreply.github.com>
2021-09-21 14:40:34 +0530

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -1,6 +1,6 @@
-/* Codded by @aju
+/* Codded by @phaticusthiccy
 Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/aju
+Instagram: www.instagram.com/kyrie.baran
 */
 
 const Asena = require('../events');
