e7fd60bd8c1f08786b8a523a376244897328d91a
Aju <89148418+aju001@users.noreply.github.com>
2021-09-24 20:20:13 +0530

    Update zara.js

--- zara.js
+++ zara.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
- Z_NUMBER: process.env.NUMBER === undefined ? '+13072881866' : process.env.NUMBER,
+ Z_NUMBER: process.env.NUMBER === undefined ? '+917994873071' : process.env.NUMBER,
  Z_OWNER: process.env.OWNER_NAME === undefined ? 'Aju ser' : process.env.OWNER_NAME,
 
 };
