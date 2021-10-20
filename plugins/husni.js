ffd83841d31bd10e5ff25d6bdb52ff7c1d8e1779
Husniser <92710588+husniser@users.noreply.github.com>
2021-10-20 21:26:05 +0530

    Delete husni.js

--- plugins/husni.js
+++
@@ -1,9 +0,0 @@
-Ende peeru husni_v2
-Enne nirmichad husni 
-Enn parayunna waste
-😎 onderth ponenki
-Google mapsil ariyoor Juma masjid 
-Nn adichal madi adinde thottappartha
-Phone number:9567749726
-😌😌😌
-Nna shari
